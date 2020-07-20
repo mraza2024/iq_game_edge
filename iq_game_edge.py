dfa2ebdba133a093673af4412136ad493a6a5243
danazzie <68479607+danazzie@users.noreply.github.com>
2020-07-20 21:01:13 +0600

    Update iq_game_edge.py

--- iq_game_edge.py
+++ iq_game_edge.py
@@ -1,2 +1,12 @@
 #how smart are you?
-print("Welcome to the Game")
+print("Welcome to the Intellectual Game! You will see 10 questions that will test your general knowledge and intelligence. Your goal is to answer all the questions correctly, otherwise the game will stop. At the end of the game, the program will show your result. Good luck!")
+
+questions_easy =["question1", "question2", "question3", "question4", "question5"]
+answers_easy=["answer1", "answer2", "answer3", "answer4", "answer5"]
+questions_medium=[
+questions_hard=[
+import random 
+print ("Question 1:" + questions_easy[random.randint(0,30)])
+answer1=input("What is your answer?)
+#if answer is right:
+              #print(
