b6c229b79f75a40c5a60639fc208958032f3a6e5
danazzie <68479607+danazzie@users.noreply.github.com>
2020-07-21 13:54:41 +0600

    Update iq_game_edge.py

--- iq_game_edge.py
+++ iq_game_edge.py
@@ -7,12 +7,16 @@ questions=["Which of these is a type of hat? (a)Sausage roll, b)Pork pie, c)Scot
                  "Which of these phrases refers to a brief success? a)Blaze in the pot, b)Spark in the tub, c)Flare in the jug, d)Flash in the pan",
                  "The young of which creature is known as a squab?  a)Salmon, b)Horse, c)Pigeon, d)Octopus,
                  "Which of these US presidents appeared on the television series 'Laugh-In?'  a)Lyndon Johnson  b)Richard Nixon  c)Jimmy Carter d)Gerald Ford"
-          "The Earth is approximately how many miles away from the Sun? a)Lyndon Johnson  b)Richard Nixon  c)Jimmy Carter d)Gerald Ford]
+          "Which insect shorted out an early supercomputer and inspired the term 'computer bug'? a)Moth  b)Roach  c)Fly  d)Japanese beetle"
+          "Which of the following men does not have a chemical element named for him? a)Albert Einstein  b)Niels Bohr  c)Isaac Newton  d)Enrico Fermi]
 answers=["b", 
               "a",
               "d", 
               "c",
-         "b"]
+         "b"
+         "a"
+        "c"
+        ]
 recomends=[]
 import random 
 x=random.randint(0,4)
@@ -26,6 +30,8 @@ if answer_right==answer_user:
     
 else:
   print("Unfortunately, your answer is wrong. However, you can i,prove your knowledge by reading or watching the next: "+recomends[x])
+  print("You have answered "+str(NUMBER OF RIGHT QUESTIONS)+" questions right.")
+ #also we need to show the user his level somehow, like 1-3 questions - average level, 4-6 - above average, 7-10 - extraordinarily smart)
   break
 
 print("Thank you for taking part in our quiz!")
