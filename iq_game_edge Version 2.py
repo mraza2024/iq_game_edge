a91085d9b0f6f5e4178d9c3521865b1b863fdbcc
danazzie <68479607+danazzie@users.noreply.github.com>
2020-07-22 14:07:27 +0600

    Update iq_game_edge Version 2.py

--- iq_game_edge Version 2.py
+++ iq_game_edge Version 2.py
@@ -1,13 +1,14 @@
 
-
+#declaring the class Question
 class Question:
     def __init__(self, mcq, answer):
         self.mcq = mcq
         self.answer = answer
-
+#introduction note
 print("Welcome to the Intellectual Game! You will see 10 questions that will test your general knowledge and intelligence.Each question will have 1 point. At the end of the game, the program will show your result.\n\n Good luck!")
 
 print()
+#list of the questions that user will see
 questions_mcqs = [
 
 "Which of these is a type of hat?\n (a)Sausage roll\n (b)Pork pie\n (c)Scotch egg\n (d)Potato crisp\n\n",
@@ -30,6 +31,7 @@ questions_mcqs = [
 
 "Now used to refer to a cat, the word 'tabby' is derived from the name of a district of what world capital?\n (a)Baghdad\n (b)Cairo\n (c)New Delhi\n (b)Moscow\n\n"]
 
+#list of the right answers for questions
 questions = [
     Question(questions_mcqs[0],"b"),
     Question(questions_mcqs[1],"a"),
@@ -42,13 +44,15 @@ questions = [
     Question(questions_mcqs[8],"a"),
     Question(questions_mcqs[9],"a")]
 
+#function that sets the grading system of our quiz: the right answer adds a point to it, the wrong answer keeps it the same
 def start_quiz(questions):
     point = 0
     for question in questions:
         answer = input(question.mcq +"Enter your answer = ")
         if answer == question.answer:
             point += 1
+#the program shows the grade of a user (number of answers he got right)
     print("Your totals points are " +str(point)+"/10")
-
+#the quiz starts when we call this function
 start_quiz(questions)
 
